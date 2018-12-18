8c71d0dc8bf3fb56b66b93ff0acbbccb2b9ca611
Patrick Hudson <phudson2@gmail.com>
2018-12-18 14:15:45 -0600

    debugging cb_client

--- cb_client/entrypoint.sh
+++ cb_client/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#/usr/bin/supervisord > /var/log/chaturbae/supervisor.log 2>&1 &
-#sleep 3
-#tail -f /var/log/chaturbae/cb_*.log
 service nginx start
-tail -f /dev/null
+/usr/bin/supervisord > /var/log/chaturbae/supervisor.log 2>&1 &
+sleep 3
+tail -f /var/log/chaturbae/cb_*.log
+#tail -f /dev/null
