f93bf8db00c99075306503333028544c9571f517
Patrick Hudson <phudson2@gmail.com>
2018-12-20 14:48:45 -0600

    generate userlist testing

--- cb_client/depoy_user.sh
+++
@@ -1,9 +0,0 @@
-sample=$(node get_18_us_list.js)
-#sample='[{"name":"foo"},{"name":"bar"}]'
-for row in $(echo "${sample}" | jq -r '.[] | @base64'); do
-    _jq() {
-     echo ${row} | base64 --decode | jq -r ${1}
-    }
-
-   echo $(_jq '.username')
-done
