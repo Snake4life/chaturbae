7b0664937ef73bb9c070d8139db47e34189fd3f0
Patrick Hudson <phudson2@gmail.com>
2018-12-20 08:55:10 -0600

    json loggin

--- cb_client/entrypoint.sh
+++ cb_client/entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-npm install -g typescript @types/node >> /var/log/chaturbae/npm_install.log 2>&1 &
-npm install >> /var/log/chaturbae/npm_install.log 2>&1 &
+echo '{"chaturbae-init": "npm install typescript - log /var/log/chaturbae/npm_install.log"}'
+npm install -g typescript @types/node >> /var/log/chaturbae/npm_install.log
+echo '{"chaturbae-init": "npm install typescript complete"}'
+echo '{"chaturbae-init": "npm install deps - log /var/log/chaturbae/npm_install.log"}'
+npm install >> /var/log/chaturbae/npm_install.log
+echo '{"chaturbae-init": "npm install deps complete"}'
+echo '{"chaturbae-init": "starting nginx - /var/log/chaturbae/supervisor.log"}'
 service nginx start > /var/log/chaturbae/supervisor.log 2>&1 &
+echo '{"chaturbae-init": "starting supervisord - /var/log/chaturbae/supervisor.log"}'
 /usr/bin/supervisord > /var/log/chaturbae/supervisor.log 2>&1 &
 sleep 3
+echo '{"chaturbae-init": "complete"}'
 tail -f /var/log/chaturbae/cb_*.log
 #tail -f /dev/null
