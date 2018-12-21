51c084f1d21c36711744737bc018ee2d71ee8809
Patrick Hudson <phudson2@gmail.com>
2018-12-21 07:31:02 -0600

    userlist generator

--- cb_client/test_userlist/get_18_us_list.js
+++ cb_client/test_userlist/get_18_us_list.js
@@ -2,7 +2,7 @@ var request = require('request');
 var cheerio = require('cheerio');
 const username = process.argv[2];
 var pattern = /^_/g;
-const url = 'https://chaturbate.com/female-cams/';
+const url = 'https://chaturbate.com/tag/18/';
 var dashRxp = new RegExp("^_", "g");
 request(url, function(error, response, html){
   var userList = []
@@ -11,7 +11,7 @@ request(url, function(error, response, html){
       const $ = cheerio.load(html);
       var count = 0;
       $('li.room_list_room').each(function(elem){
-        if(count < 50){
+        //if(count < 50){
         var username = $(this).attr('data-sl');
 
         var noLeading = username.replace(pattern, '');
@@ -21,14 +21,15 @@ request(url, function(error, response, html){
         //console.log(result).
         userList.push({username: `${username}`, username_sanitized: `${noPost}`})
         //console.log(`${username} ${noPost}`);
-        count = count + 1
-      } else {
+        //count = count + 1
+      //} else {
     // userList = userList.slice(10, userList.length);
 
+
+      //}
+      });
       console.log(JSON.stringify(userList))
       process.exit(0);
-      }
-      });
     }
 
 });
