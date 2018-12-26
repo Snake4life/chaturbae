0650cb4747d08a035135e16527fb60c5a3b23524
Patrick Hudson <phudson2@gmail.com>
2018-12-26 08:26:18 -0600

    compose update

--- cb_client/bash_scripts/is_naked.sh
+++ cb_client/bash_scripts/is_naked.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 inputFile=$1
-curl -s -F "file=@${inputFile}" 'http://watcher1.backend.chaturbae.tv:5000' | jq -r '.[]'
+curl -s -F "file=@${inputFile}" 'http://watcher4.backend.chaturbae.tv:5000' | jq -r '.[]'
