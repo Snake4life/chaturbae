939e9355ca7c46359279cbea886b4f87d20f9ec2
Patrick Hudson <phudson2@gmail.com>
2018-12-26 07:36:30 -0600

    logging changes

--- cb_client/test_userlist/get_18_us_list.js
+++ cb_client/test_userlist/get_18_us_list.js
@@ -11,7 +11,7 @@ request(url, function(error, response, html){
       const $ = cheerio.load(html);
       var count = 0;
       $('li.room_list_room').each(function(elem){
-        if(count < 100){
+        if(count < 5){
         var username = $(this).attr('data-sl');
         var noLeading = username.replace(pattern, '');
         var noUnder = noLeading.replace(/_/g, '-')
